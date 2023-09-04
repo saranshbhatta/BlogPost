d46e7f54d544a3f91156230152d3562135608be1
SaranshBhatta <nitesh44bhatta@gmail.com>
2023-09-04 14:16:22 +0545

    added new api

--- BlogProject/server/BlogProject/RepoLayer/IBlogRepo.cs
+++ BlogProject/server/BlogProject/RepoLayer/IBlogRepo.cs
@@ -5,8 +5,11 @@ namespace BlogProject.RepoLayer
     public interface IBlogRepo
     {
         Task<List<BlogModel>> GetAllPostsWithCategories();
-        Task<string>CreateNewPost(BlogModel blogObj);
+        Task<string> CreateNewPost(BlogModel blogObj);
         Task<BlogModel> GetPostById(int postId);
+        Task<string> CreateNewCategory(CategoryModel CategoryObj);
+        Task<List<CategoryModel>> GetCategoryWithFilter(CategoryModel model);
+
 
     }
 }
